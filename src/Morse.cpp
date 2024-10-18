09ba9e4755ffe600f95a77e7387a65016ac24058
William Crépault <william.crepault@gmail.com>
2024-10-17 22:33:57 -0400

    création et implémentation class Morse.cpp

---
+++ src/Morse.cpp
@@ -0,0 +1,31 @@
+#include "Morse.h"
+
+Morse::Morse() {
+    ;
+}
+
+void Morse::allumerDELInterne(short p_duree) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(p_duree);
+}
+void Morse::eteindreDELInterne(short p_duree) {
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(p_duree);
+}
+
+void Morse::executerSelonValeurIndex(String p_valeurIndexTab) {
+  for (byte i = 0; i < p_valeurIndexTab.length(); ++i) {
+    // caractère court.
+    if (p_valeurIndexTab[i] == 'c') {
+      allumerDELInterne(UNITE);
+    }
+    // caractère long.
+    else {
+      allumerDELInterne(UNITE * 3);      
+    }
+    // Éteindre unite * 1 entre les parties d'une lettre
+    eteindreDELInterne(UNITE);
+  }
+  // Ajouter éteindre * 2 pour avoir unité * 3 entre les lettres
+  eteindreDELInterne(UNITE * 2);
+}
