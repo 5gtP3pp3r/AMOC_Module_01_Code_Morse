693b697c48ae700e13c2791ac92d45c84efc4b15
5gtp3pp3r <william.crepault@gmail.com>
2024-10-26 16:38:00 -0400

    Modification des tableaux de string en byte et utisation dans la méthode exécuterSelonValeurIndex() pour prende un byte et non un string

--- include/Morse.cpp
+++ include/Morse.cpp
@@ -4,18 +4,18 @@ Morse::Morse() {
   ;
 }
 
-void Morse::convertirStringEnMorse(String &p_entree) {
+void Morse::convertirStringEnMorse(const String &p_entree) {
   for (byte i = 0; i < p_entree.length(); ++i) {  
     // Le caractère est une lettre: 
     if (toLowerCase(p_entree[i]) >= 'a' && toLowerCase(p_entree[i]) <= 'z') {
       // valeur du caractère - valeur de 'a' = sa position dans le tableau tabAlphabet. Exemples: 'a'(97) - 'a'(97) = 0, 'a' est bien à l'index 0 et'z'(122) - 'a'(97) = 25.
-      String valeurIndexAlpha = TAB_ALPHABET[toLowerCase(p_entree[i]) - 'a'];
+      byte valeurIndexAlpha = TAB_ALPHABET[toLowerCase(p_entree[i]) - 'a'];
       executerSelonValeurIndex(valeurIndexAlpha);
     }
     // Le caractère est un chiffre:
     else if (p_entree[i] >= '0' && p_entree[i] <= '9') {
       // Valeur du caractère - valeur de '0'= sa position dans le tableau TabNumerique. Exemples: '0' - '0' = 0 et '9' - '0' = 9.
-      String valeurIndexNumerique = TAB_NUMERIQUE[p_entree[i] - '0'];
+      byte valeurIndexNumerique = TAB_NUMERIQUE[p_entree[i] - '0'];
       executerSelonValeurIndex(valeurIndexNumerique);
     } 
     else {    
@@ -29,28 +29,51 @@ void Morse::convertirStringEnMorse(String &p_entree) {
   eteindreDELInterne(SEPT_UNITES);
 }
 
-void Morse::executerSelonValeurIndex(String p_valeurIndexTab) {
-  for (byte i = 0; i < p_valeurIndexTab.length(); ++i) {
-    // caractère court "c".
-    if (p_valeurIndexTab[i] == 'c') {
+void Morse::executerSelonValeurIndex(byte p_valeurIndexTab) const {
+  // "accesseur" est la valeur au début du byte, elle me donne donc le nombre d'itération à
+  // faire dans la partie valeur du byte pour "accéder" aux valeurs 0 ou 1 à afficher.
+  // L'accesseur est isolé grace à l'opérateur ">>" qui éliminera les 5 premiers bite de "p_valeurIndexTab".
+  byte accesseur = p_valeurIndexTab >> POSITION;
+
+  // "positionValeur" est un chiffre qui me donnera access au début des bites de la section  
+  // "valeur" de "p_valeurIndexTab". Il sera décrémenter pour lire les bites de gauche à droite 
+  // dans la section "valeur". Il est donc logiquement 1 de moins que la position de l'accesseur.
+  byte positionValeur = accesseur -1 ;
+
+  // "valeurBite" est donc le bite isolé à la position présenté par "positionValeur".
+  byte valeurBite = p_valeurIndexTab >> positionValeur;
+
+  for (byte i = 0; i < accesseur; ++i) {
+
+    // Isolation du dernier bite de valeurBite grace à un "&" logique avec un masque égale à 0000 0001
+    valeurBite = valeurBite & MASQUE_VALEUR;
+    // caractère court "." -> 0.
+    if (valeurBite == 0) {
       allumerDELInterne(UNITE);
     }
-    // caractère long "l".
+    // caractère long "-" -> 1.
     else {
       allumerDELInterne(TROIS_UNITES);      
     }
     // Éteindre unite * 1 entre les parties d'une lettre
     eteindreDELInterne(UNITE);
+
+    // Décrémentation de "positionValeur" pour changer de bite à isoler depuis "p_valeurIndexTab".
+    --positionValeur;
+    // Réatribution d'une nouvelle valeur à "valeurBite" grace à l'opérateur ">>" 
+    // depuis "p_valeurIndexTab" avec la nouvelle valeur de "positionValeur".
+    // "valeurBite sera remasqué en début de boucle pour isoler le bite suivant."
+    valeurBite = p_valeurIndexTab >> positionValeur;
   }
   // Ajouter éteindre * 2 pour avoir unité * 3 entre les lettres
   eteindreDELInterne(DEUX_UNITES);
 }
 
-void Morse::allumerDELInterne(short p_duree) {
+void Morse::allumerDELInterne(short p_duree) const {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(p_duree);
 }
-void Morse::eteindreDELInterne(short p_duree) {
+void Morse::eteindreDELInterne(short p_duree) const {
   digitalWrite(LED_BUILTIN, LOW);
   delay(p_duree);
 }
--- include/Morse.h
+++ include/Morse.h
@@ -1,32 +1,34 @@
 #pragma once
 #include <Arduino.h>
 /*
-    1- The length of a dot is one unit.
-    2- A dash is three units.
-    3- The space between parts of the same letter is one unit.
-    4- The space between letters is three units.
-    5- The space between words is seven units.
+    
+                                LOGIQUE MORSE EN BYTE
 
-A . _           U . . _
-B _ . .         V . . . _
-C _ . _ .       W . _ _
-D _ . .         X _ . . _
-E .             Y _ . _ _
-F . . _ .       Z _ _ . . 
-G _  _ .
-H . . . .
-I . .
-J . _ _ _
-K _ . _         1 . _ _ _ _
-L . _ . .       2 . . _ _ _
-M _ _           3 . . . _ _
-N _ .           4 . . . . _
-O _ _ _         5 . . . . .
-P . _ _ .       6 _ . . . .
-Q _ _ . _       7 _ _ . . .
-R . _ .         8 _ _ _ . .
-S . . .         9 _ _ _ _ .
-T _             0 _ _ _ _ _
+                                [accesseur]   -> Nombre de bites contenants des valeurs
+                                [non utilisé] -> Bites ignorés
+                                [valeur]      -> Valeurs équivalentes "." = 0, "-" = 1
+
+                                                                               [accesseur] [non utilisé] [valeur]
+A . _         01     U . . _          001         "A" -> byte = 0b01000001 ->     [010]        [000]        [01]
+B _ . . .   1000     V . . . _       0001         "B" -> byte = 0b10001000 ->     [100]          [0]      [1000]
+C _ . _ .   1010     W . _ _          011         "C" -> byte = 0b10001010 ->     [100]          [0]      [1010]
+D _ . .      100     X _ . . _       0110         "D" -> byte = 0b01100100 ->     [011]         [00]       [100]
+E .            0     Y _ . _ _       1011         "E" -> byte = 0b00100000 ->     [001]       [0000]         [0]
+F . . _ .   0010     Z _ _ . .       1100
+G _ _ .      110
+H . . . .   0000
+I . .         00
+J . _ _ _   0111
+K _ . _      101     1 . _ _ _ _     01111        "1" -> byte = 0b10101111 ->     [101]                  [01111]
+L . _ . .   0100     2 . . _ _ _     00111
+M _ _         11     3 . . . _ _     00011
+N _ .         10     4 . . . . _     00001
+O _ _ _      111     5 . . . . .     00000        "5" -> byte = 0b10100000 ->     [101]                  [00000]
+P . _ _ .   0110     6 _ . . . .     10000
+Q _ _ . _   1101     7 _ _ . . .     11000
+R . _ .      101     8 _ _ _ . .     11100
+S . . .      000     9 _ _ _ _ .     11110
+T _            1     0 _ _ _ _ _     11111        "0" -> byte = 0b10111111 ->     [101]                  [11111]
 
 */
 class Morse {
@@ -34,24 +36,29 @@ class Morse {
 public:
 
 Morse();
-void convertirStringEnMorse(String &p_entree);
-void executerSelonValeurIndex(String p_valeurIndexTab);
-void allumerDELInterne(short p_duree);
-void eteindreDELInterne(short p_duree);
+void convertirStringEnMorse(const String &p_entree);
+void executerSelonValeurIndex(byte p_valeurIndexTab) const;
+void allumerDELInterne(short p_duree) const;
+void eteindreDELInterne(short p_duree) const;
 
-//                               a      b       c       d     e     f       g      h      i      j       k      l      m     
-const String TAB_ALPHABET[26] = {"cl", "lccc", "lclc", "lcc", "c", "cclc", "llc", "cccc", "cc", "clll", "lcl", "clcc", "ll",
-//                               n      o      p       q       r      s     t     u      v       w      x       y       z
-                                "lc", "lll", "cllc", "llcl", "clc", "ccc", "l", "ccl", "cccl", "cll", "lccl", "lcll", "llcc"};
-//                                   0        1        2        3        4        5        6        7        8        9        
-const String TAB_NUMERIQUE[10] = {"lllll", "cllll", "cclll", "cccll", "ccccl", "ccccc", "lcccc", "llccc", "lllcc", "llllc"};
-/*  J'ai essayé de faire un tableau de "Byte", mais j'ai rencontré un problème. Prenons en exemple les lettres e, i, s et h. 
-    Elle auraient les valeurs 0b0, 0b00, 0b000 et 0b0000. se qui revient au final à 0. donc je me retrouve avec un mauvais 
-    affichage. La même chose est vrais aussi pour les valeur qui commence par 0 comme a 0b01 au final ça valeur sera 1 et 
-    sera identique à la valeur de u 0b001 soit 1 aussi. J'ai besoin d'une structure qui conserve toutes les valeurs déclarées.
-    Je crois donc que la string reste la meilleur option puisque je suis capable de différencier chaque valeurs c et l.*/
+//                                 a           b           c           d           e           f           g           h           i           j           k           l           m     
+const byte TAB_ALPHABET[26] = {0b00100001, 0b10001000, 0b10001010, 0b01100100, 0b00100000, 0b10000010, 0b01100110, 0b10000000, 0b01000000, 0b10000111, 0b01100101, 0b10000100, 0b01000011,
+//                                 n           o           p           q           r           s           t           u           v           w           x           y           z
+                               0b01000010, 0b01100111, 0b10000110, 0b10001101, 0b01100010, 0b01100000, 0b00100001, 0b01100001, 0b10000001, 0b01100011, 0b10001001, 0b10001011, 0b10001100};
+//                                  0           1           2           3           4           5           6           7           8           9        
+const byte TAB_NUMERIQUE[10] = {0b10111111, 0b10101111, 0b10100111, 0b10100011, 0b10100001, 0b10100000, 0b10110000, 0b10111000, 0b10111100, 0b10111110};
+
+/*  1- Temps d'un point est un unité.
+    2- Temps d'un trait est trois unités.
+    3- Temps entre chaque valeurs d'une lettre ou chiffre est un unité.
+    4- Temps entre chaque lettre ou chiffre est trois unités
+    5- Temps entre chaque mots ou monbre est sept unités
+*/
 const short UNITE = 200;
 const short DEUX_UNITES = UNITE * 2;
 const short TROIS_UNITES = UNITE * 3;
 const short SEPT_UNITES = UNITE *7;
+
+const byte POSITION = 5;
+const byte MASQUE_VALEUR = 0b00000001;
 };
