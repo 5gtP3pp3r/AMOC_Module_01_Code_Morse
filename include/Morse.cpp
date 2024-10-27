23f00ce9b5489cbcf6593092657ed7dec9b31eea
5gtp3pp3r <william.crepault@gmail.com>
2024-10-26 20:37:21 -0400

    mis à jours espaces et commentaires

--- include/Morse.cpp
+++ include/Morse.cpp
@@ -6,26 +6,23 @@ Morse::Morse() {
 
 void Morse::convertirStringEnMorse(const String &p_entree) {
   for (byte caractere = 0; caractere < p_entree.length(); ++caractere) {  
-    // Le caractère est une lettre: 
-    if (toLowerCase(p_entree[caractere]) >= 'a' && toLowerCase(p_entree[caractere]) <= 'z') {
+    
+    if (toLowerCase(p_entree[caractere]) >= 'a' && toLowerCase(p_entree[caractere]) <= 'z') { // Si le caractère est une lettre: 
       // valeur du caractère - valeur de 'a' = sa position dans le tableau tabAlphabet. Exemples: 'a'(97) - 'a'(97) = 0, 'a' est bien à l'index 0 et'z'(122) - 'a'(97) = 25.
       byte valeurIndexAlpha = TAB_ALPHABET[toLowerCase(p_entree[caractere]) - 'a'];
       executerSelonValeurIndex(valeurIndexAlpha);
-    }
-    // Le caractère est un chiffre:
-    else if (p_entree[caractere] >= '0' && p_entree[caractere] <= '9') {
-      // Valeur du caractère - valeur de '0'= sa position dans le tableau TabNumerique. Exemples: '0' - '0' = 0 et '9' - '0' = 9.
+    }   
+    else if (p_entree[caractere] >= '0' && p_entree[caractere] <= '9') {  // Si le caractère est un chiffre:
+      // Valeur du caractère - valeur de '0'= sa position dans le tableau TabNumerique. Exemples: '0'(48) - '0'(48) = 0 et '9'(57) - '0'(48) = 9.
       byte valeurIndexNumerique = TAB_NUMERIQUE[p_entree[caractere] - '0'];
       executerSelonValeurIndex(valeurIndexNumerique);
     } 
-    else {    
-      // Le caractère est un espace, éteindre unité * 7 entre les mots.  
+    else {  // Sinon le caractère est un espace ou autre non pris en concidération, éteindre unité * 7 entre les mots.          
       eteindreDELInterne(SEPT_UNITES);
     }
-  }
-  // Montre visuellement la fin de la chaine avec: éteint 1400 ms, allumé 1400 ms et éteint 1400 ms.
+  } 
   eteindreDELInterne(SEPT_UNITES);
-  allumerDELInterne(SEPT_UNITES);
+  allumerDELInterne(SEPT_UNITES); // Montre visuellement la fin de la chaine avec: éteint 1400 ms, allumé 1400 ms et éteint 1400 ms.
   eteindreDELInterne(SEPT_UNITES);
 }
 
@@ -37,8 +34,8 @@ void Morse::executerSelonValeurIndex(byte p_valeurIndexTab) const {
                                                                                                         
   for (byte position = accesseur -1; position >= 0; --position) {     
     // "valeurBites" est une plage de bites isolées grace à l'opération bit à bit >> "position".
-    // Cette plage contiendra la valeur du bit à isoler qui sera masqué plus tard.                        // Exemples des opérations dans  
-    // Au départ, position est donc logiquement 1 de moins que l'accesseur. "position"                    // la boucle avec la lettre "a" 0b01000001
+    // Cette plage contiendra la valeur du bit à isoler qui sera masqué plus tard.                        // Exemples des opérations dans la  
+    // Au départ, position est donc logiquement 1 de moins que l'accesseur. "position"                    // boucle avec la lettre "a" 0b01000001
     // sera décrémenté à chaque tour pour isoler le prochain bit vers la droite.                          // accesseur = 0b01000001 >> 5 = 0b/00000/[010] donc 2 tours
     byte valeurBites = p_valeurIndexTab >> position;                                                      // Tour 1: valeurBites = 0b01000001 >> (0b00000001 (1)) = 0b/0/010000[0]
     // Isolation du dernier bit de "valeurBites" grace à un "&" logique avec un masque de 0000 0001       // bite = 0b00[1]0000[0] & 0b00[0]0000[1] = 0b00[0]0000[0] donc 0
